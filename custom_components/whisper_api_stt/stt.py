4efbaa665a78e7fd75850f285c5f1a050c0cbd19
Sanjeev Dwivedi <sanjeev.dwivedi@gmail.com>
2025-06-11 11:43:11 -0700

    Update stt.py

--- custom_components/whisper_api_stt/stt.py
+++ custom_components/whisper_api_stt/stt.py
@@ -26,7 +26,7 @@ import io
 
 CONF_API_KEY = 'api_key'
 DEFAULT_LANG = 'en-US'
-OPENAI_STT_URL = "https://api.openai.com/v1/audio/transcriptions"
+OPENAI_STT_URL = "http://sanjeev-debian-llm-vm.lan:9000/v1/audio/transcriptions"
 CONF_MODEL = 'model'
 CONF_URL = 'url'
 CONF_PROMPT = 'prompt'
